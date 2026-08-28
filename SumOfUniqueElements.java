25f28e33cd47a43e0b776fbee659ff1543f52b2d
shivanisingh080308-hue <shivanisingh080308@gmail.com>
2026-08-28 06:47:56 +0530

    Add SumOfUniqueElements class with sumOfUnique method
    Implement a method to calculate the sum of unique elements in an array.

---
+++ SumOfUniqueElements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+  public class SumOfUniqueElements {
+        public int sumOfUnique(int[] nums) {
+            int res = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+                if (map.get(nums[i]) == 1) res += nums[i];
+                else if (map.get(nums[i]) == 2) res -= nums[i];
+            }
+            return res;
+        }
+        public static void main(String[]args){
+            SumOfUniqueElements obj = new SumOfUniqueElements();
+            int[] nums={1,2,3,2};
+            int ans = obj.sumOfUnique(nums);
+          System.out.println("Result: " + ans);
+    }
+}
